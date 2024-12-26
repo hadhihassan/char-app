cf59b1ad22bb6097fc2506ce5809d45962cffbbd
hadhi hassan m <diluhadhi01@gmail.com>
2024-12-26 13:20:32 +0530

    feat: Signup api implimented, added user model and utils(jwt token gen fn)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,35 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from './lib/db.js';
 
 import authRoutes from './routes/auth.route.js';
 
 dotenv.config()
-const app = express()
-
 const PORT = process.env.PORT
 
+const app = express()
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
 app.use('/api/auth', authRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    // app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    // });
+}
 
 app.listen(PORT, () => {
     console.log("Server is runing on 5001");

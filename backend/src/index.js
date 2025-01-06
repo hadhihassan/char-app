870cf1f069ad027993e53629a250031bd93c2729
hadhi hassan m <diluhadhi01@gmail.com>
2025-01-06 22:12:45 +0530

    Project Compeleted.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv'
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from 'path'
 
 import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
@@ -25,13 +26,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
-// if (process.env.NODE_ENV === "production") {
-    // app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    // app.get("*", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    // });
-// }
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log("Server is runing on 5001");
